b05566de9220b7644e3fd26e17f7701583ed5afd
elpatosilva <33077026+elpatosilva@users.noreply.github.com>
2022-06-22 16:30:04 -0300

    Add files via upload

---
+++ resources/lib/constants.py
@@ -0,0 +1,29 @@
+API_VERSION = '5.1'
+API_KEY = 'c3RhciZhbmRyb2lkJjEuMC4w.cNMxdgOjN9t9gJ-h-8KIAO7h_PnjWs2BWWh56Zhj5zc'
+CONFIG_URL = 'https://bam-sdk-configs.bamgrid.com/bam-sdk/v3.0/star-22bcaf0a/android/v6.2.0/google/tv/prod.json'
+PAGE_SIZE_SETS = 15
+PAGE_SIZE_CONTENT = 30
+SEARCH_QUERY_TYPE = 'ge'
+BAM_PARTNER = 'star'
+
+WATCHLIST_SET_ID = 'aa7770e2-8162-4620-9a44-42057929af98'
+WATCHLIST_SET_TYPE = 'WatchlistSet'
+CONTINUE_WATCHING_SET_ID = '76aed686-1837-49bd-b4f5-5d2a27c0c8d4'
+CONTINUE_WATCHING_SET_TYPE = 'ContinueWatchingSet'
+HUBS_SET_ID = 'e3abff32-0325-41e2-be1d-c9dc1f556f44'
+HUBS_SET_TYPE = 'getDictionaryLatest'
+
+HEADERS = {
+    'User-Agent': 'BAMSDK/v6.2.0 (star-22bcaf0a 2.0.0.0; v3.0/v6.2.0; android; phone) samsung SM-G965N (star2ltexx-user 5.1.1 QP1A.190711.020 500200819 release-keys; Linux; 5.1.1; API 22)',
+    'x-application-version': 'google',
+    'x-bamsdk-platform-id': 'android',
+    'x-bamsdk-client-id': 'star-22bcaf0a',
+    'x-bamsdk-platform': 'android',
+    'x-bamsdk-version': '6.2.0',
+    'Accept-Encoding': 'gzip',
+}
+
+RATIO_ASK = 0
+RATIO_IMAX = 1
+RATIO_WIDESCREEN = 2
+RATIO_TYPES = [RATIO_ASK, RATIO_IMAX, RATIO_WIDESCREEN]
